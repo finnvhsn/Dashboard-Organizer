0ec361d2b94fbbebc16a37da7d7c16df7455a72e
Finn von Heesen <finnvhsn@gmail.com>
2024-02-15 14:00:15 +0100

    Standard Templates and py files

--- project/views.py
+++ project/views.py
@@ -1,6 +1,9 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/dashboard')
+def home():
+    return render_template("test.html")
 
 
-
-
-
-#store views and urls 
