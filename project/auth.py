ccaf3be92e43f4e1cd32b74d2ea56450e267c78d
DeepPurple2002 <maximilianschmidt107@gmail.com>
2024-04-04 14:12:06 +0200

    Layout Veränderung Front End

--- project/auth.py
+++ project/auth.py
@@ -7,22 +7,23 @@ auth = Blueprint('auth', __name__)
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     
-    if request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form.get('email')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+    if request.method == 'POST':match
+    username = request.form.get('username')
+    
+    email = request.form.get('email')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
         
-        if len(username) < 3:
+    if len(username) < 3:
             flash("Username must be at least 3 characters.")
-        elif '@' not in email:
+    elif '@' not in email:
             flash("Email must include @")
-        elif password1 is not None and len(password1) < 4:
+    elif password1 is not None and len(password1) < 4:
             flash("password has to be at least 4 characters long.")
-        elif password2 != password1:
+    elif password2 != password1:
             flash("Passwords must be the same.")
             
-        else:
+    else:
 
             add_user(username, email, password1)
             flash("User added successfully")
