c8abd6b65d4dd1b4d948d1388785088495c75070
Rafael Guaraldo <rafael.guaraldo@hpe.com>
2024-04-09 11:35:25 +0200

    Spaces News updated to V4, new route "/all"

--- project/views.py
+++ project/views.py
@@ -8,6 +8,24 @@ views = Blueprint('views', __name__)
 def landingpage():
     return render_template("landingpage.html")
 
+@views.route("/all")
+def page_user4():
+    #Calls the returned value of fetch_stock_data function
+    finances = fetch_stock_data() #Stockdata
+    
+    # Calls the get_weather_data function to fetch weather data
+    weather_data = get_weather_data()
+    
+    #Calls the returned value of fetch_space_news function
+    articles = fetch_space_news()
+
+    #Calls the returned value of fetch_f1_results function
+    f1_results = fetch_f1_results() 
+    
+    #TODO Notizen fehlen
+    return render_template("test.html", finances=finances, weather_data=weather_data, articles=articles, f1_results= f1_results)  
+
+
 @views.route("/finn")
 def page_user1():
     #Calls the returned value of fetch_stock_data function
